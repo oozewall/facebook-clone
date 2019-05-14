0836c811f0bbf742c4b2b31224e43065a5cbb28a
prazeev <ujwal_55@yahoo.com>
2019-05-14 19:39:58 +0545

    boilerplate

---
+++ server.js
@@ -0,0 +1,11 @@
+const app = require('express')()
+const port = process.env.PORT || 3000
+
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.listen(port, ()=>{
+    console.log(`App running on port ${port}`)
+})
